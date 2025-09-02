f6cc0464899da0d5f5ed5cd7ba473c7b69076253
willow <42willow@pm.me>
2025-09-02 20:26:30 +1000

    fix: window drag regions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,7 +22,8 @@
 </script>
 
 <nav class="fixed top-0 grid h-7 w-full grid-cols-[1fr_auto_1fr] items-center bg-ctp-mantle text-sm text-ctp-subtext0">
-  <div class="flex">{@render drag()}</div>
+  <div data-tauri-drag-region class="h-full">&nbsp</div>
+
   <DropdownMenu.Root bind:open={dropdownOpen}>
     <DropdownMenu.Trigger
       class="m-1 flex items-center justify-center gap-1 rounded-sm px-1 transition-colors duration-300 {!!credentials
@@ -46,29 +47,31 @@
     </DropdownMenu.Portal>
   </DropdownMenu.Root>
 
-  <div class="flex items-center pr-1.5">
-    {@render drag()}
-    {#if !!credentials.state.auth}
-      {#if credentials.state.status.type === "synced"}
-        <CloudCheck class="size-5" />
-      {:else if credentials.state.status.type === "syncing"}
-        <Cloud class="size-5 animate-pulse" />
-      {:else if credentials.state.status.type === "offline"}
-        <CloudOff class="size-5 stroke-ctp-red" />
-      {:else}
-        <CloudAlert class="size-5 stroke-ctp-red" />
+  <div class="flex h-full items-center pr-1.5">
+    <div data-tauri-drag-region class="h-full flex-grow">&nbsp;</div>
+
+    <div>
+      {#if !!credentials.state.auth}
+        {#if credentials.state.status.type === "synced"}
+          <CloudCheck class="size-5" />
+        {:else if credentials.state.status.type === "syncing"}
+          <Cloud class="size-5 animate-pulse" />
+        {:else if credentials.state.status.type === "offline"}
+          <CloudOff class="size-5 stroke-ctp-red" />
+        {:else}
+          <CloudAlert class="size-5 stroke-ctp-red" />
+        {/if}
       {/if}
-    {/if}
+    </div>
   </div>
 </nav>
-<div class="h-screen">
-  <hr class="h-7" />
-  {@render children()}
-</div>
 
-{#snippet drag()}
-  <span data-tauri-drag-region class="flex-grow"></span>
-{/snippet}
+<div class="flex h-screen flex-col overflow-hidden">
+  <hr class="h-7" />
+  <div class="flex-1 overflow-auto">
+    {@render children()}
+  </div>
+</div>
 
 {#snippet dropdownItem(/** @type {string} */ href, /** @type {string} */ name, /** @type {string[]} */ kbd = [])}
   <DropdownMenu.Item class="rounded-md px-2 py-1 hover:bg-ctp-surface0">
