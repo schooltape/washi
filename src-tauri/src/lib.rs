3f3ae164ceb04bad3da5e206b733840ce50bad19
willow <42willow@pm.me>
2025-09-03 15:20:14 +1000

    refactor: simplify lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,25 +1,17 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let mut builder = tauri::Builder::default();
+
     #[cfg(desktop)]
     {
-        builder = builder.plugin(tauri_plugin_single_instance::init(|_app, argv, _cwd| {
-        println!("a new app instance was opened with {argv:?} and the deep link event was already triggered");
-        // when defining deep link schemes at runtime, you must also check `argv` here
-    }));
+        builder = builder.plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
     }
+
     builder
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
