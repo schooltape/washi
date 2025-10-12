9368e46dadc4c919a5540dbd48e74d7956a774f1
willow <42willow@pm.me>
2025-10-12 17:47:12 +1100

    refactor: update tab styling

--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -19,12 +19,12 @@
 <Tabs.Root bind:value={selectedItem} class="flex flex-col gap-4">
   {#if showTabs}
     <Tabs.List
-      class="flex gap-2 overflow-x-scroll rounded-lg border border-ctp-surface0 bg-ctp-crust p-2 text-sm leading-[0.01em] font-semibold">
+      class="flex gap-2 rounded-lg border border-ctp-surface0 bg-ctp-crust p-2 text-sm leading-[0.01em] font-semibold">
       {#each Object.entries(items) as [id, label]}
         <Tabs.Trigger
           {...triggerProps}
           value={id}
-          class="h-8 flex-grow rounded-md border-1 border-ctp-surface0 bg-ctp-base px-2 py-2 transition-colors duration-300 data-[state=active]:border-(--ctp-accent) data-[state=active]:bg-(--ctp-accent)/20 data-[state=active]:text-ctp-text"
+          class="h-8 flex-grow rounded-md border-1 border-ctp-surface0 bg-ctp-mantle px-2 py-2 transition-colors duration-300 data-[state=active]:border-(--ctp-accent) data-[state=active]:bg-(--ctp-accent)/20 data-[state=active]:text-ctp-text"
           >{label}</Tabs.Trigger>
       {/each}
     </Tabs.List>
